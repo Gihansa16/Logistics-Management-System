0f8414ac6af2adc2d8de0e98cb8684a58e565ea6
Gihansa16 <dakshilagihansa@gmail.com>
2025-10-25 02:16:16 +0530

    Corrected mistakes for add city function

--- main.c
+++ main.c
@@ -9,27 +9,20 @@ void addCities(int maxCities,char cityNames[][30],int *cityCount);
 
 int main()
 {
- int choice,c2;
+    char cityNames[MAX_CITIES][30];
+    int cityCount=0;
 
-    do
+    int choice,c2;
+
+     do
     {
         printf("\n----Logistics Management System----\n");
         printf("1. Manage Cities\n");
-        printf("6.Exit\n");
-        printf("Enter your choice:");
-        scanf("%d",&choice);
+        printf("6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-        switch(choice)
-        {
-        case 1:
-            do
-            {
-                printf("\n---City Management---\n");
-                printf("5.Back\n");
-                printf("Enter choice:");
-                scanf("%d",&c2);
-
-                switch (choice)
+        switch (choice)
         {
         case 1:
             do
@@ -40,58 +33,48 @@ int main()
                 printf("Enter choice: ");
                 scanf("%d", &c2);
 
-
-
-                switch(c2)
+                switch (c2)
                 {
                 case 1:
                     addCities(MAX_CITIES, cityNames, &cityCount);
                     break;
-
                 case 5:
                     break;
                 default:
                     printf("Invalid choice!\n");
                 }
             }
-            while(c2!=5);
-
-        case 6:
-            printf("Exiting program...\n");
+            while (c2 != 5);
             break;
-
         default:
             printf("Invalid choice!\n");
         }
 
     }
-    while(choice!=6);
+    while (choice != 6);
 
     return 0;
 }
 
-void addCities(int maxCities, char cityNames[][30], int *cityCount)
-{
-    char newCity[30];
-    int i = 0;
 
-    if (*cityCount >= maxCities)
-    {
-        printf("\nCannot add more cities. Maximum limit reached.\n");
-        return;
-    }
+        void addCities(int maxCities,char cityNames[][30],int *cityCount)
+        {
+            char newCity[30];
+            int i=0;
 
-    printf("\nEnter city name: ");
-    scanf(" %[^\n]", newCity);
-
-
-    while (newCity[i] != '\0')
-    {
-        cityNames[*cityCount][i] = newCity[i];
-        i++;
-    }
-    cityNames[*cityCount][i] = '\0';
-    (*cityCount)++;
-
-    printf("City added successfully!\n");
-}
+            if(*cityCount>= maxCities)
+            {
+                printf("\nCannot add more cities.Maximum limit reached.\n");
+                return;
+            }
+            printf("\nEnter city name:");
+            scanf(" %[^\n]",newCity);
+            while(newCity[i]!='\0')
+            {
+                cityNames[*cityCount][i]=newCity[i];
+                i++;
+            }
+            cityNames[*cityCount][i]='\0';
+            (*cityCount)++;
+            printf("City added successfully!\n");
+        }
